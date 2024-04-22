beebd02a4672809cf5244aa4a9cb5e58d4068c69
potalzz <tprhkd1607@naver.com>
2024-04-22 23:06:17 +0900

    terminal test

---
+++ hex.js
@@ -0,0 +1,23 @@
+const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+const getRandomNumber = () => {
+  return Math.floor(Math.random() * hex.length);
+};
+
+const makeHexColor = () => {
+  let hexColor = "#";
+  for (let i = 0; i < 6; i++) {
+    hexColor += hex[getRandomNumber()];
+  }
+  console.log(hexColor);
+  return hexColor;
+};
+
+btn.addEventListener("click", () => {
+  let HexColor = makeHexColor();
+  document.body.style.backgroundColor = HexColor;
+  color.textContent = HexColor;
+});
